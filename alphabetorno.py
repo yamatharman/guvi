c541463aa6aee20f761b3dd2992dd618f814777d
yamatharman <42862839+yamatharman@users.noreply.github.com>
2019-02-07 12:03:24 +0530

    Update alphabetorno.py

--- alphabetorno.py
+++ alphabetorno.py
@@ -1,9 +1,8 @@
-print("Enter '0' for exit.");
-ch = input("Enter any character: ");
-if ch == '0':
-    exit();
+l = input("Input a letter of the alphabet: ")
+
+if l in ('a', 'e', 'i', 'o', 'u'):
+	print("%s is a vowel." % l)
+elif l == 'y':
+	print("Sometimes letter y stand for vowel, sometimes stand for consonant.")
 else:
-    if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
-    	print(ch, "is an alphabet.");
-    else:
-    	print(ch, "is not an alphabet.");
+	print("%s is a consonant." % l) 
