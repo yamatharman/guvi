0c30cd50a841822c3dbca9bd6007704e4e3a1c9c
yamatharman <42862839+yamatharman@users.noreply.github.com>
2019-02-07 11:54:39 +0530

    Update vowelsorconstant.py

--- vowelsorconstant.py
+++ vowelsorconstant.py
@@ -1,6 +1,5 @@
 ch = input("Enter a character: ")
-if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'
- or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
+if(ch=='a' or ch =='e' or ch=='i' or ch=='o' or ch=='u'):
     print(ch, "is a Vowel")
 else:
     print(ch, "is a Consonant")
