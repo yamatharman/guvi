106e825c92d9d42758f392408e86902b885d3883
yamatharman <42862839+yamatharman@users.noreply.github.com>
2019-02-08 10:32:22 +0530

    Update 9nk.py

--- 9nk.py
+++ 9nk.py
@@ -1,8 +1,8 @@
 sum=0
-a=input().split()
-b=input().split()
-n=int(a[0])
-k=int(a[1])
-for j in range(0,k):
-  sum=sum+int(b[j])
+x=input().split()
+y=input().split()
+n=int(x[0])
+m=int(x[1])
+for i in range(0,m):
+  sum=sum+int(y[i])
 print(sum)
