c6fade49d1a4b76b6c0798176286c17d047f9fc8
yamatharman <42862839+yamatharman@users.noreply.github.com>
2019-02-08 14:24:27 +0530

    Update alphabetornot.py

--- alphabetornot.py
+++ alphabetornot.py
@@ -1,5 +1,5 @@
-l = input("Input a letter of the alphabet: ")
-if l in ('a', 'e', 'i', 'o', 'u'):
-	print("%s is a vowel." % l)
-elif l == 'y':
-	print("%s is a consonant." % l) 
+ch=input()
+if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
+  print("Alphabet")
+else:
+  print("No")
