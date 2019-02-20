3351ab1af30a62c052ba9a385679f0b6b33cf47b
yamatharman <42862839+yamatharman@users.noreply.github.com>
2019-02-20 11:18:28 +0530

    Create 18interval.py

---
+++ 18interval.py
@@ -0,0 +1,13 @@
+#interval.py
+a=input().split()
+b=int(a[0])
+c=int(a[1])
+for num in range(b,c):
+  temp=num
+  sum=0
+  while temp>0:
+      digit=temp%10
+      sum=sum+digit**3
+      temp=temp//10
+      if sum==num:
+           print (num)
